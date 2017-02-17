e14435508da568d6bd4736883a17b31dfbbf8c4e
Aashil Patel <aashilpatel1993@gmail.com>
2017-02-17 16:04:35 -0500

    Create a loop which runs every 15 mins.
    TODO: Take into account a scenario where two issues being created
    with only the latest issue being labeled as "easy". Now when someone
    labels the older one with "easy", the given code would not honor that
    issue.

--- github-issue-tracker.py
+++ github-issue-tracker.py
@@ -1,9 +1,21 @@
 import urllib2
 import json
+import datetime as dt
+from time import sleep
 
-url = "https://api.github.com/repos/scikit-learn/scikit-learn/issues?labels=easy"
+labels = "easy"
+url = "https://api.github.com/repos/scikit-learn/scikit-learn/issues?labels="+labels
+old_time = dt.datetime.utcfromtimestamp(0).isoformat()
 
-resp = urllib2.urlopen(url).read()
-json_resp = json.loads(resp)
+while True:
 
-print type(json_resp)
+	resp = urllib2.urlopen(url).read()
+	json_resp = json.loads(resp)
+
+	new_time = json_resp[0]["created_at"]
+
+	if new_time > old_time:
+		old_time = new_time
+		print "A new easy issue has been created at "+ old_time
+
+	sleep(60*15)
