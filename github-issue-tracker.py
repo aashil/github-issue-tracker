4f10838c3cfedd500ceeca1318cbd62e1f3f2833
Aashil Patel <aashilpatel1993@gmail.com>
2017-02-25 18:24:54 -0500

    Add EmailGun API to notify the users via email.
    * Reads API details from email-api.conf file.

--- github-issue-tracker.py
+++ github-issue-tracker.py
@@ -1,12 +1,23 @@
 import urllib2
 import json
 import datetime as dt
+import os
+import configparser
 from time import sleep
 
 labels = "easy"
 url = "https://api.github.com/repos/scikit-learn/scikit-learn/issues?labels="+labels
 begin_time = dt.datetime.utcnow().isoformat()
 issues = set()
+
+config = configparser.ConfigParser()
+config.read('email-api.conf')
+username = config.get('api-data', 'username')
+passkey = config.get('api-data', 'passkey')
+server = config.get('api-data', 'server')
+email_from = config.get('api-data', 'from')
+email_to = "Aashil Patel <aashilpatel1993@gmail.com>"
+subject = "Github Issue Tracker"
 
 while True:
 
@@ -22,11 +33,27 @@ while True:
 
 		issues.add(data["number"])
 
-		if data["created_at"] > old_time:
+		if data["created_at"] > begin_time:
 			issue_count += 1
-			old_time = data["created_at"]
+			begin_time = data["created_at"]
 
 	if issue_count > 0:
-		print str(issue_count) + " new issues available"
+
+		message = ""
+
+		if issue_count == 1:
+			message = str(issue_count) + " new issue available"
+		elif issue_count > 1:
+			message = str(issue_count) + " new issues available"
+
+		#SQL injection exploits this technique. Should be improved in future
+		command = "curl -s --user " + "'" + username+":"+passkey + "' "
+		command +=  server + " "
+		command += "-F from='"+ email_from + "' "
+		command += "-F to='" + email_to + "' "
+		command += "-F subject='" + subject + "' "
+		command += "-F text='" + message + "'"
+
+		os.system(command)
 
 	sleep(15*60)
